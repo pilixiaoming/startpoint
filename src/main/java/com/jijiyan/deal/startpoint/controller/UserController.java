c8d5338e9af6eecca05b4a589f4f1bd20a60a1f1
pilixiaoming <pilixiaoming@gmail.com>
2015-07-31 12:51:05 +0800

    spring mvc

---
+++ src/main/java/com/jijiyan/deal/startpoint/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.jijiyan.deal.startpoint.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.google.common.collect.Maps;
+import com.jijiyan.deal.startpoint.model.User;
+
+@Controller
+@RequestMapping(value="/user")
+public class UserController {
+	Map<String, User> users = Maps.newHashMap();
+	@RequestMapping(value = "/users", method = RequestMethod.GET)
+	public String list(Model model) {
+		
+		users.put("yuandaxia", new User("袁大侠"));
+		users.put("yuanxiaoxia", new User("袁小侠"));
+		users.put("yuanxiake", new User("袁侠客"));
+		model.addAttribute("users", users);
+		return "user/list";
+	}
+	
+/*
+ * version 1
+ * @RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String add(Model model) {
+		model.addAttribute(new User());
+		return "user/add";
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String adda(User user) {
+		users.put(user.getName(), user);
+		return "redirect:/user/users";
+	}
+ * 
+ */
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String add(@ModelAttribute("user")User user) {
+		return "user/add";
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String adda(User user) {
+		users.put(user.getName(), user);
+		return "redirect:/user/users";
+	}
+}
--- src/main/java/com/jijiyan/deal/startpoint/model/User.java
+++ src/main/java/com/jijiyan/deal/startpoint/model/User.java
@@ -3,6 +3,13 @@ package com.jijiyan.deal.startpoint.model;
 public class User {
 	private String name;
 
+	public User() {
+	}
+
+	public User(String name) {
+		this.name = name;
+	}
+
 	public String getName() {
 		return name;
 	}
